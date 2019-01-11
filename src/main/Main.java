360b40a31a1955db6d98b6e70cb4b30f86793642
ashutoshupp99 <ashupp99@gmail.com>
2019-01-11 15:16:13 +0530

    second commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,21 @@
 package main;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String args[]){
-        System.out.println("hello world");}
+    public static void main(String args[]) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter your name");
+        String name = null;
+        try {
+            name = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        StringBuffer word = new StringBuffer(name);
+
+        System.out.println(name);
+    }
 }
+    
